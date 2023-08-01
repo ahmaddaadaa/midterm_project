db27289157e0e7d0d1f13e78f1688d745dc11a23
Colin Spangelo <colin.spangelo@gmail.com>
2023-07-31 22:51:43 -0600

    set up seed for categories table.

--- db/seeds/03_categorys.sql
+++ db/seeds/03_categorys.sql
@@ -1,11 +1,12 @@
 INSERT INTO categories (type, description)
-VALUES ('Kitchen Appliences', 'Appliances of all sorts and sizes for your kitchen!'),
-('Kitchen Appliences', 'Appliances of all sorts and sizes for your kitchen!'),
-('Sports Wear', 'All coll!'),
-('Toys and Games', 'Appliances of all sorts and sizes for your kitchen!'),
-('Electronics', 'Appliances of all sorts and sizes for your kitchen!'),
-('Books', 'Appliances of all sorts and sizes for your kitchen!'),
-('Kids Wear', 'Appliances of all sorts and sizes for your kitchen!'),
-('Mens Wear', 'Appliances of all sorts and sizes for your kitchen!'),
-('Shoes', 'Appliances of all sorts and sizes for your kitchen!'),
-('Womens Wear', 'Appliances of all sorts and sizes for your kitchen!'),
+VALUES ('Kitchen Appliences', 'Appliances of all sorts and sizes for the kitchen!'),
+('House Hold Appliences', 'Appliances of all sorts and sizes suited for your every house hold need!'),
+('Toys and Games', 'Toys and Games for children up to 16!'),
+('Electronics', 'Video games, consoles and more!'),
+('Movies', 'DVDs Blue-ray and even retro VHS!'),
+('Books', 'All types of books ranging from picture books to Sun Tzu!'),
+('Kids Wear', 'Shirts, pants, shorts, socks, shoes for children!'),
+('Mens Wear', 'Shirts, pants, shorts, socks, shoes for Men!'),
+('Womens Wear', 'Shirts, pants, shorts, socks, shoes for Women!'),
+('Sports Wear', 'Slick breathable wicking clothes!');
+
