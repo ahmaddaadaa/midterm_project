102c7bc00bbb51b9927e2573cd0e31c8c98d8093
Colin Spangelo <colin.spangelo@gmail.com>
2023-08-02 03:06:26 -0600

    The console log in the input-product is not logging. I'm stopping now because it wouldn't beneift staying up later.

--- public/scripts/input-product-scripts.js
+++ public/scripts/input-product-scripts.js
@@ -2,37 +2,33 @@
 
 $(document).ready(function() {
 
-  $(":button").click(function(){
-    $.get("/putItem", function(data, status){
-      alert("Data: " + data + "\nStatus: " + status);
-    });
-  });
+  $("form").submit(function(event) {
+    const name = $("#nameInput").val().trim();
+    const category_id = $("#typeInput").val().trim()
+    const price = $("#priceInput").val().trim()
+    const description = $("#descriptionInput").val().trim()
+    const photo_url = $("#urlInput").val().trim()
+    event.preventDefault();
+
+    console.log(name, category_id, price, description, photo_url)
 
   const createProductRecord = function() {
     //Counter starts after last seeded user
     let user_id = 11;
-    const name = document.getElementById("nameInput").value;
-    const category_id = document.getElementById("typeInput").value;
-    const price = document.getElementById("priceInput").value;
-    const description = document.getElementById("descriptionInput").value;
-    const photo_url = document.getElementById("urlInput").value;
+
     let is_sold = false;
     return db
       .query(
         `INSERT INTO products (user_id, name, price, category_id, description, photo_url, is_sold)
-    VALUES ($1, $2, $3, $4, $5, $6, $7, $8);`, [id, user_id, name, category_id, price, description, photo_url, is_sold])
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8);`, [user_id, name, category_id, price, description, photo_url, is_sold])
     .then((result) => {
-
     id++;
     user_id++;
     })
-    .catch(error);
+    .catch((error)=> {
+      console.log(error)
+    });
   };
 
-
-
-
 });
-module.exports ={
-  createProductRecord
-}
+})
