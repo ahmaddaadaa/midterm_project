b2899b8af6e77e858665cdfd1d3c6899bf4b16dc
Colin Spangelo <colin.spangelo@gmail.com>
2023-08-02 11:48:49 -0600

    Have not fully tested add to favorites function but pretty confident it is right.

--- db/queries/favorite_items.js
+++ db/queries/favorite_items.js
@@ -1,8 +1,24 @@
 const db = require('../connection');
 
-const getFavoriteItems = () => {
+const addFavoriteItems = (user_id, favoriteProduct) => {
+
+  return db.query(`
+  INSERT INTO favorite_items (user_id, product_id)
+  VALUES ($1, $2)
+  RETURNING *;`, [user_id, favoriteProduct])
+    .then((data) => {
+      return data.rows[0];
+    }).catch((error) => {
+      console.log(error)
+    });
+};
+
+
+
+const showFavoriteItems = (user_id) => {
   return db.query(`
   SELECT * FROM favorite_items
+  JOIN products ON
 
 
   ;`)
@@ -11,4 +27,8 @@ const getFavoriteItems = () => {
     });
 };
 
-module.exports = { getFavoriteItems };
+
+module.exports = {
+  addFavoriteItems,
+  showFavoriteItems
+ };
