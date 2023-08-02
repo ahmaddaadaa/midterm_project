b679342362ce961c2d7bf295d3be94e1fd67065f
ahmaddaadaa <ahmadd@uvic.ca>
2023-08-01 20:35:40 -0700

    generated data

---
+++ db/seeds/04_favorite_items.sql
@@ -0,0 +1,12 @@
+INSERT INTO favorite_items (user_id, product_id)
+VALUES
+  (1, 3),
+  (1, 5),
+  (2, 1),
+  (2, 4),
+  (3, 8),
+  (3, 10),
+  (4, 7),
+  (4, 9),
+  (5, 2),
+  (5, 6);
---
+++ db/seeds/06_reviews.sql
@@ -0,0 +1,12 @@
+INSERT INTO reviews (user_id, product_id, rating, comments)
+VALUES
+  (1, 1, 5, 'Great smartphone with excellent camera.'),
+  (1, 3, 4, 'Love the design of this summer dress.'),
+  (2, 2, 4, 'Powerful laptop for all my tasks.'),
+  (2, 5, 5, 'Effective skincare set for my skin.'),
+  (3, 4, 4, 'Easy-to-use coffee maker.'),
+  (3, 8, 5, 'Spacious and durable camping tent.'),
+  (4, 3, 5, 'High-quality cookware set for my kitchen.'),
+  (4, 7, 4, 'Good performance from the tennis racket.'),
+  (5, 6, 3, 'The formal shirt fits well.'),
+  (5, 10, 4, 'Convenient hair dryer for everyday use.');
