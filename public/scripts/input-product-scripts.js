f13e4ad5b884fda3e02d694f3a1a9befacf3e4cc
Colin Spangelo <colin.spangelo@gmail.com>
2023-08-02 09:29:48 -0600

    Having trouble linking everything together. Currently cannot get the console.log to show that I had placed inside the promise for the route.

--- public/scripts/input-product-scripts.js
+++ public/scripts/input-product-scripts.js
@@ -1,34 +1,16 @@
-
-
 $(document).ready(function() {
 
   $("form").submit(function(event) {
     const name = $("#nameInput").val().trim();
-    const category_id = $("#typeInput").val().trim()
-    const price = $("#priceInput").val().trim()
-    const description = $("#descriptionInput").val().trim()
-    const photo_url = $("#urlInput").val().trim()
+    const category_id = $("#typeInput").val().trim();
+    const price = $("#priceInput").val().trim();
+    const description = $("#descriptionInput").val().trim();
+    const photo_url = $("#urlInput").val().trim();
     event.preventDefault();
 
-    console.log(name, category_id, price, description, photo_url)
+    console.log(name, category_id, price, description, photo_url);
 
-  const createProductRecord = function() {
-    //Counter starts after last seeded user
-    let user_id = 11;
-
-    let is_sold = false;
-    return db
-      .query(
-        `INSERT INTO products (user_id, name, price, category_id, description, photo_url, is_sold)
-    VALUES ($1, $2, $3, $4, $5, $6, $7, $8);`, [user_id, name, category_id, price, description, photo_url, is_sold])
-    .then((result) => {
-    id++;
-    user_id++;
-    })
-    .catch((error)=> {
-      console.log(error)
-    });
-  };
+    createProductRecord(name, category_id, price, description, photo_url, is_sold);
+  });
 
 });
-})
--- routes/input-product.js
+++ routes/input-product.js
@@ -2,27 +2,24 @@ const db = require('../db/connection');
 const express = require('express');
 const router = express.Router();
 
-// const{ showProducts } = require("../db/queries/products")
 
-
+const{ createProductRecord } = require("../db/queries/inputNewProduct")
 //      /api
 router.get("/putItem", (req, res) => {
 
-return db
+const values = createProductRecord()
   .then(data => {
-
+    console.log("I am inside the returned promise")
     res.json(data.rows);
-    console.log("products: "+ data.rows);
+    console.log("new Record : "+ data.rows);
     return data.rows;
   })
-  .catch(error =>{console.log(error)});
+  .catch(error =>{
+    // console.log(error)
+  });
 
   return res
 })
-
-
-
-
 
 
 module.exports = router;
