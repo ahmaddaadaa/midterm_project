d43a35d9d5c25e5456a445fdaa6f242c47d9185e
Colin Spangelo <colin.spangelo@gmail.com>
2023-08-02 21:04:01 -0600

    Elbert spent a significant time with us helping us understanding a variety of concepts and applying them to our filters function so it can be used by our app. Some concepts included AJAX, Jquery, url encoding and get request limitations.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,23 +1,29 @@
 const db = require('../db/connection');
 const express = require('express');
 const router = express.Router();
+const { filterPriceAndSearch } = require("../db/queries/filterAndSearch")
 
 const{ showProducts } = require("../db/queries/products")
 
 
 //      /api
 router.get("/", (req, res) => {
+console.log(req.query)
+filterPriceAndSearch(req.query).then((data)=>{
+  console.log("-----------!!!!!!!!!!!!!--------------")
+  res.json(data);
+console.log(data)
+});
+//   const values = showProducts()
+//   .then(data => {
+//     // console.log("values", values);
 
-  const values = showProducts()
-  .then(data => {
-    // console.log("values", values);
-    res.json(data.rows);
-    console.log("products: "+ data.rows);
-    return data.rows;
-  })
-  .catch(error =>{console.log(error)});
+//     console.log("products: "+ data.rows);
+//     return data.rows;
+//   })
+//   .catch(error =>{console.log(error)});
 
-  return res
+//   return res
 })
 
 module.exports = router;
