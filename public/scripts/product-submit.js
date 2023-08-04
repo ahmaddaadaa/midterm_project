3d82ae822b0321291cda78b4bb3c8dd1d5c791ba
ahmaddaadaa <ahmadd@uvic.ca>
2023-08-03 19:43:36 -0700

    disables the code

--- public/scripts/product-submit.js
+++ public/scripts/product-submit.js
@@ -1,17 +1,17 @@
 
 
-$(document).ready(function() {
-  $("#create-item-form").submit(function(event) {
-    event.preventDefault();
-    alert("Hello")
-    const name = $("#nameInput").val().trim();
-    const type = $("#typeInput").val().trim();
-    const price = $("#priceInput").val().trim();
-    const description = $("#descriptionInput").val().trim();
-    const photo_url = $("#urlInput").val().trim();
-    const is_sold = false;
+// $(document).ready(function() {
+//   $("#create-item-form").submit(function(event) {
+//     event.preventDefault();
+//     alert("Hello")
+//     const name = $("#nameInput").val().trim();
+//     const type = $("#typeInput").val().trim();
+//     const price = $("#priceInput").val().trim();
+//     const description = $("#descriptionInput").val().trim();
+//     const photo_url = $("#urlInput").val().trim();
+//     const is_sold = false;
 
-    console.log(name, type, price, description, photo_url);
+//     console.log(name, type, price, description, photo_url);
 
-  });
-});
+//   });
+// });
