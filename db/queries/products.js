2657f0304ad05a8c3ed50e06aacd2233d6a44153
ahmaddaadaa <ahmadd@uvic.ca>
2023-08-03 19:37:11 -0700

    added options.product_name to the function showProducts

--- db/queries/products.js
+++ db/queries/products.js
@@ -1,12 +1,19 @@
 const db = require('../connection');
 
 
-const showProducts = () => {
-  return db.query(`
-  SELECT * FROM products
-  ORDER BY products.id DESC
-  LIMIT 10
-  ;`)
+const showProducts = (options) => {
+  let queryString = `SELECT * FROM products
+  WHERE 1=1 `;
+  let paramsArray =[];
+  if(options.product_name){
+    queryString += ` AND name = $1 `
+    paramsArray.push(options.product_name)
+  }
+  queryString += ` ORDER BY products.id DESC LIMIT 10 `
+  return db.query(queryString,paramsArray)
+  .then((res) => {
+    return res.rows;
+  })
     
 };
 
