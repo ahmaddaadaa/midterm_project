101670f3fa0553b6c0c2dfc64b317179daad33a2
ahmaddaadaa <ahmadd@uvic.ca>
2023-08-03 19:44:26 -0700

    addes /products route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,24 +6,53 @@ const { filterPriceAndSearch } = require("../db/queries/filterAndSearch")
 const{ showProducts } = require("../db/queries/products")
 
 
-//      /api
-router.get("/", (req, res) => {
-console.log(req.query)
+
+
+
+// router.post("/products/filter", (req, res) => {
+//   console.log("inside post reuest!!!!!!!");
+//   console.log("req.body:",req.body);
+//   filterPriceAndSearch(req.body)
+//   .then((data) => {
+//     return res.json(data);
+    
+//   })
+  
+
+// })
+
+
+
+    // /api
+router.get("/products", (req, res) => {
+console.log("inside API route!!!",req.query)
+showProducts(req.query)
+  .then((data) => {
+    return res.json(data);
+    
+  })
+
+return;
+
+
+
+
+
 filterPriceAndSearch(req.query).then((data)=>{
   console.log("-----------!!!!!!!!!!!!!--------------")
   res.json(data);
 console.log(data)
 });
-//   const values = showProducts()
-//   .then(data => {
-//     // console.log("values", values);
+  const values = showProducts()
+  .then(data => {
+    // console.log("values", values);
 
-//     console.log("products: "+ data.rows);
-//     return data.rows;
-//   })
-//   .catch(error =>{console.log(error)});
+    console.log("products: "+ data.rows);
+    return data.rows;
+  })
+  .catch(error =>{console.log(error)});
 
-//   return res
+  return res
 })
 
 module.exports = router;
