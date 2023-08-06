a575321e50c64d14bf69b95dd43fd884a214fb13
ahmaddaadaa <ahmadd@uvic.ca>
2023-08-05 23:19:26 -0700

    Addded new post and get API routes for adding a missing 'favorites' feature

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,62 +1,46 @@
-const db = require('../db/connection');
-const express = require('express');
+const db = require("../db/connection");
+const express = require("express");
 const router = express.Router();
-const { filterPriceAndSearch } = require("../db/queries/filterAndSearch")
 
-const{ showProducts } = require("../db/queries/products")
-
-
-
+const { showProducts } = require("../db/queries/products");
+const { getProductId } = require("../db/queries/get_product_id");
+const { addFavoriteItems, showFavoriteItems } = require("../db/queries/favorite_items");
 
 
-// router.post("/products/filter", (req, res) => {
-//   console.log("inside post reuest!!!!!!!");
-//   console.log("req.body:",req.body);
-//   filterPriceAndSearch(req.body)
-//   .then((data) => {
-//     return res.json(data);
-
-//   })
-
-
-// })
-
-
-
-    // /api
+// /api
 router.get("/products", (req, res) => {
-console.log("inside API route!!!",req.query)
-showProducts(req.query)
-  .then((data) => {
-    if(req.data){
-      return filterPriceAndSearch(req.data)
-    }
+  console.log("inside API route!!!", req.query);
+  showProducts(req.query).then((data) => {
     return res.json(data);
-
-
-  })
-
-return;
-
-
-
-
-
-filterPriceAndSearch(req.query).then((data)=>{
-  console.log("-----------!!!!!!!!!!!!!--------------")
-  res.json(data);
-console.log(data)
+  });
 });
-  const values = showProducts()
-  .then(data => {
-    // console.log("values", values);
 
-    console.log("products: "+ data.rows);
-    return data.rows;
-  })
-  .catch(error =>{console.log(error)});
+router.post("/favorites", (req, res) => {
+  const productName = req.body.productName;
+  console.log("inside favorites!!!", productName);
 
-  return res
-})
+  getProductId(productName)
+    .then((data) => {
+      return addFavoriteItems(1, data.id);
+    })
+    .then(() => {
+      res.json({ message: "Item has been added" });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ error: "An error occurred" });
+    });
+});
+
+// for user.id = 1
+router.get("/favorites", (req, res) => {
+  showFavoriteItems(1).then((data) => {
+    return res.json(data);
+  });
+});
+
+
+
+
 
 module.exports = router;
