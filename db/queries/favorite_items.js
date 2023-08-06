60156541d7516b10618bf29c93a5feb6356e7c91
ahmaddaadaa <ahmadd@uvic.ca>
2023-08-05 23:11:16 -0700

    Implemented SQL query for retrieving product IDs of favorited products

--- db/queries/favorite_items.js
+++ db/queries/favorite_items.js
@@ -3,33 +3,40 @@ const db = require('../connection');
 //The input should be a button on the icon on the main page?
 
 const addFavoriteItems = (user_id, product_id) => {
-
-  return db.query(`
-  INSERT INTO favorite_items (user_id, product_id)
-  VALUES ($1, $2)
-  RETURNING *;`, [user_id, product_id])
+  return db.query(
+    `
+    INSERT INTO favorite_items (user_id, product_id)
+    VALUES ($1, $2)
+    RETURNING *;
+    `,
+    [user_id, product_id]
+  )
     .then((data) => {
       return data.rows[0];
-    }).catch((error) => {
-      console.log(error)
+    })
+    .catch((error) => {
+      console.log(error);
     });
 };
+
 
 
 
 const showFavoriteItems = (user_id) => {
   return db.query(`
-  SELECT * FROM favorite_items
-  JOIN products ON product_id = products.id
-  WHERE user_id = $1
-  LIMIT 10
-  ;`)
-    .then(data => {
+    SELECT name, price, type, description, photo_url, is_sold, date 
+    FROM products
+    JOIN favorite_items ON favorite_items.product_id = products.id
+    WHERE favorite_items.user_id = $1;
+  `, [user_id])
+    .then((data) => {
       return data.rows;
-    }).catch((error)=>{
-      console.log(error)
+    })
+    .catch((error) => {
+      console.log(error);
     });
 };
+
 
 
 module.exports = {
